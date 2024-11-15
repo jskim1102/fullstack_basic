2cbdbcb27f5a8e9151067cf191d3b0a419bb76a0
jskim1102 <96895094+jskim1102@users.noreply.github.com>
2024-11-15 15:44:30 +0900

    Delete myapp directory

--- myapp/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Person(Base):
-    __tablename__ = "collection_jinsoo"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    age = Column(Integer, nullable=False)
